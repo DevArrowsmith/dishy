fcb197eda2c55a2a5ea59215b403b5490a4e1209
devarrowsmith <devarrowsmith@gmail.com>
2021-09-12 15:08:18 +0100

    Create React app and remove obsolete content. Create directory astructure. Create test file for App component with snapshot test.

---
+++ src/tests/App.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from '../components/App';
+
+describe("App component", () => {
+  test("Snapshot test", () => {
+    const { asFragment } = render(
+      <App />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
