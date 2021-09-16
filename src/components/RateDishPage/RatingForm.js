a155a52e43c17743451959f0f8d93519bb88c9f8
devarrowsmith <devarrowsmith@gmail.com>
2021-09-16 21:42:00 +0100

    Update RatingForm component with rating slider. Add keys to mapped components.

--- src/components/RateDishPage/RatingForm.js
+++ src/components/RateDishPage/RatingForm.js
@@ -1,17 +1,41 @@
+import { useState } from "react";
+
 const RatingForm = ({ restaurantsList, foodsList }) => {
+
+  const initialState = {
+    rating: 10
+  }
+
+  const [rating, setRating] = useState(initialState.rating);
+
+  const handleRatingChange = (event) => {
+    setRating(event.target.value)
+  };
+
+
   if (restaurantsList.length > 0) {
     console.log(restaurantsList);
     return (
       <>
         <p>Choose your restaurant:</p>
         <select>
-          {restaurantsList.map(restaurant => <option value={restaurant.name}>{restaurant.name}</option>)};
+          {restaurantsList.map(restaurant => <option value={restaurant.name} key={restaurant.id}>{restaurant.name}</option>)};
         </select>
         <p>What did you eat?</p>
         <select>
-          {foodsList.map(food => <option value={food}>{food}</option>)}
+          {foodsList.map(food => <option value={food} key={food}>{food}</option>)}
         </select>
-        <p>How was it?</p>
+        <p>Rate it!</p>
+        <input 
+          type="range"
+          id="rating"
+          name="rating"
+          min="1"
+          max="10"
+          step="1"
+          onChange={handleRatingChange}
+        />
+        <p>Your rating: {rating}</p>
       </>
     )
   } else {
