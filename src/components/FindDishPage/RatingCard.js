badc408495d8627d5bc5a190555af054ccf344b5
Hugh Kristiansen <kaarelinden@hotmail.com>
2021-09-20 17:04:49 +0100

    average score rounded to nearest decimal place

--- src/components/FindDishPage/RatingCard.js
+++ src/components/FindDishPage/RatingCard.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types'
 function RatingCard({ rating }) {
   const [showScore, setShowScore] = useState(false)
   const scores = rating.scores
-  const averageScore = rating.averageScore;
+  const averageScore = Math.round(rating.averageScore * 10) / 10
+  
 
   const handleScore = (e) => {
     e.preventDefault()
@@ -17,7 +18,6 @@ function RatingCard({ rating }) {
       {rating.scores.length>1 ? 
         <div>{showScore ? rating.scores.map(score => <div>{score}</div>) : averageScore}<button onClick={handleScore}>click</button></div> 
         : <div>{rating.scores[0]}</div>}
-      
     </div>
   )
 }
