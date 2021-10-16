7082505c36ddf63acd7aa6ba5bb3ba8116f1d092
devarrowsmith <devarrowsmith@gmail.com>
2021-10-16 12:45:46 +0100

    Update navbar following burger menu creation. Remove navbar links except from Home link. Style user welcome message.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,6 @@ import PropTypes from "prop-types";
 import BurgerMenu from "./BurgerMenu/BurgerMenu";
 import "../../styles/Navbar.css";
 import homeImage from "../../assets/dishy-logo-1.png";
-import reviewImage from "../../assets/review-image-1.png";
-import searchImage from "../../assets/search-image-1.png";
 
 const Navbar = ({ user, setUser }) => {
   const handleLogout = (e) => {
@@ -25,34 +23,17 @@ const Navbar = ({ user, setUser }) => {
             alt="Home"
           />
         </Link>
+
         <div id="Navbar-right-links-container">
           {user.username ? (
-            <>
-              hello {user.username}
-              <button type="submit" onClick={handleLogout}>
-                logout
-              </button>
-              <Link to="/review">
-                <img
-                  className="Navbar-image"
-                  id="Navbar-review-image"
-                  src={reviewImage}
-                  alt="Rate a Dish"
-                />
-              </Link>
-            </>
-          ) : (
-            <Link to="/signin">log in</Link>
-          )}
-
-          <Link to="/find">
-            <img
-              className="Navbar-image"
-              id="Navbar-search-image"
-              src={searchImage}
-              alt="Find a Dish"
-            />
-          </Link>
+            <div
+              className="Navbar-text-container"
+              id="Navbar-username-container"
+            >
+              <p>Hi</p>
+              <p>{user.username}!</p>
+            </div>
+          ) : null}
 
           <BurgerMenu isLoggedIn={user.username} handleLogout={handleLogout} />
         </div>
